49a86e77f2d4c331847f22c82e433fecee1b8381
AjjRibera <110746567+GashaKasha@users.noreply.github.com>
2023-04-09 13:51:47 -0700

    inital commit with spring boot practice one

---
+++ Projects/spring-practice-one/src/test/java/com/compositionbook/springpracticeone/SpringPracticeOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compositionbook.springpracticeone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
